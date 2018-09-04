3ffc28f6f1627187cb416cdfce829b34f697fb05
PHenrique152 <phvr2014@gmail.com>
2018-09-04 07:57:33 -0300

    Update bd.sql

--- bd.sql
+++ bd.sql
@@ -1,7 +1,7 @@
 use MaisCP2;
 
 CREATE TABLE aluno(
-aluno_id int identity not null,
+aluno_id int not null,
 serie int not null,
 curso int not null,
 PRIMARY KEY (aluno_id),
@@ -9,14 +9,14 @@ FOREIGN KEY(aluno_id) REFERENCES usuario(usuario_id)
 );
 
 CREATE TABLE professor(
-professor_id int identity not null,
+professor_id int not null,
 disciplina int not null,
 PRIMARY KEY (professor_id),
 FOREIGN KEY (professor_id) REFERENCES usuario(usuario_id)
 );
 
 CREATE TABLE usuario(
-usuario_id int identity not null,
+usuario_id int auto_increment not null,
 nome varchar(30) not null,
 dataNasc date not null,
 matricula varchar(9) not null,
@@ -26,20 +26,20 @@ PRIMARY KEY(usuario_id)
 );
 
 CREATE TABLE thread(
-thread_id int identity not null,
+thread_id int auto_increment not null,
 titulo varchar(30) not null,
 disciplina int not null,
 data date not null,
 serie int not null,
-descricao varchar not null,
+descricao varchar(2048) not null,
 usuario_id int not null,
 PRIMARY KEY (thread_id),
 FOREIGN KEY (usuario_id) REFERENCES usuario(usuario_id)
 );
 CREATE TABLE postagem(
-postagem_id int identity not null,
+postagem_id int auto_increment not null,
 data date not null,
-resposta varchar not null,
+resposta varchar(2048) not null,
 usuario_id int not null,
 thread_id int not null,
 PRIMARY KEY (postagem_id),
