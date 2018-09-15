a9c7ec3d42c3e05bdc41e8218c130d7fe01d0b68
PHenrique152 <phenrique152@outlook.com>
2018-09-15 16:55:06 -0300

    tela de login

---
+++ index/inicio.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>MaisCP2 - Aprendendo Além da Escola</title>
+  <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="temas/padrao/css/style.css">
+  <link rel="icon" href="favi.ico" type="image/ico" sizes="64x64">
+
+</head>
+
+<div class="header">
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="esquerda">
+        <a href="index.php"  title="maiscp2.com" class="TextoLink"><img src="logoLink.png" class="logoImagem">MaisCP2</a>
+      </div>
+
+      <div class="direita">
+        <a  class= "btn btn-primary botao" href="index.php/pergunta" title="MaisCP2.com">Adicionar pergunta</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<body>
+  <div class="container fundo">
+    <div class="row">
+        <div class="col-lg-12">
+          <a href="a">Início</a> <i class="fa fa-chevron-circle-right paginationArrow "></i>
+          <a href="a">Matérias</a> <i class="fa fa-chevron-circle-right  paginationArrow "></i>
+          <a href="a">Responder</a> <i class="fa fa-chevron-circle-right  paginationArrow "></i>
+          <a href="a">Notificações</a> <i class="fa fa-chevron-circle-right  paginationArrow "></i>
+
+        </div>
+    </div>
+
+    <div class="row">
+
+      <div class="col-lg-8">
+      </div>
+
+      <div class="col-lg-4">
+      </div>
+
+    </div>
+  </div>
+
+</body>
+
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" type="text/javascript"> </script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"> </script>
+</html>
