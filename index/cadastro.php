e9da1bf27b725044e828baaed32d10c4a337d6b4
Pedro Henrique <phenrique152@outlook.com>
2018-09-18 09:13:55 -0300

    mudanças

---
+++ index/cadastro.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <meta charset="UTF-8"/>
+  <title>MaisCPII - Aprendendo além da escola</title>
+  <link rel="icon" href="favi.ico" type="image/ico" sizes="64x64">
+  <link rel="icon" href="imagens/favi.ico" type="image/ico" sizes="64x64">
+  <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+   <link rel="stylesheet" type="text/css" href="styleCadastro.css">
+</head>
+
+<body>
+  <div id="login-box">
+
+
+    <div id="login-logo">
+      <img src="maiscp2.jpg" id="logo" alt="+CP2">
+    </div>
+
+    <p id="cadastre"><b>Cadastre-se</p></b>
+    <p id="tipo"> Escolha seu tipo de usuário: </p>
+
+
+
+
+      <!--Criar uma conta-->
+
+    <a href="mais_CP2_aluno.php" id="linkAluno"><button class ="butaoAluno btn bt primarybutton" type="button">Aluno</button></a>
+
+  </br><a href="mais_CP2_aluno.php" id="linkAluno"><button class ="butaoProfessor btn bt primarybutton" type="button">Professor</button></a>
+
+
+
+      </form>
+
+
+
+  </div>
+
+</body>
+
+</html>
