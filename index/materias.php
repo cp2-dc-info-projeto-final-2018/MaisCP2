fff7a11f79d2437328f3e78b9096e2bb83f51c3f
rosa <rosasheeran284@gmail.com>
2018-10-02 09:18:06 -0300

    imagem materia
    imagens

---
+++ index/materias.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>MaisCP2 - Aprendendo Além da Escola</title>
+  <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="CSS/styleInicio.css">
+  <link rel="icon" href="Imagens/favi.ico" type="image/ico" sizes="64x64">
+
+
+</head>
+
+<div class="header">
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="esquerda">
+        <a href="index.php"  title="maiscp2.com" class="navBar TextoLink"><img src="Imagens/logoLink.png" class="logoImagem">MaisCP2</a>
+
+        <a class= "navBar TextoLink" href="a">Início</a>
+        <a class= "navBar TextoLink" href="a">Responder</a>
+        <a class= "navBar TextoLink" href="a">Notificações</a>
+        <form class="searchBar" action="/action_page.php">
+          <input class="textBusca"  type="text" placeholder="Pesquisar" name="pesquisar">
+          <button type="submit"><i class="search fa fa-search"></i></button>
+        </form>
+
+
+      </div>
+
+
+
+      <div class="direita">
+        <a  class= "btn btn-primary botao" href="index.php/pergunta" title="MaisCP2.com">Sair</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12  forumMod forumMargin">
+
+                  <h1>Selecione a matéria</h1>
+
+
+
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+.mySlides {display:none;}
+</style>
+<body>
+
+
+
+<div class="w3-content w3-display-container">
+  <div class="w3-display-container mySlides">
+  <a href=”http://localhost:8080/MaisCP2/pagina.php”>  <img src="Imagens/soci.jpg" style="width:100%"> </a>
+    <div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">
+      Sociologia
+    </div>
+  </div>
+
+
+  <div class="w3-display-container mySlides">
+      <a href=”http://localhost:8080/MaisCP2/pagina.php”><img src="Imagens/des.jpg" style="width:100%"></a>
+    <div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">
+      Desenho
+    </div>
+  </div>
+
+  <div class="w3-display-container mySlides">
+      <a href=”http://localhost:8080/MaisCP2/pagina.php”><img src="Imagens/geo.jpg" style="width:100%"></a>
+    <div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">
+      Geografia
+    </div>
+  </div>
+
+  <div class="w3-display-container mySlides">
+      <a href=”http://localhost:8080/MaisCP2/pagina.php”><img src="Imagens/ing.png" style="width:100%"></a>
+    <div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">
+      Inglês
+    </div>
+  </div>
+
+
+  <div class="w3-display-container mySlides">
+      <a href=”http://localhost:8080/MaisCP2/pagina.php”><img src="Imagens/quim.jpg" style="width:100%"></a>
+    <div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">
+      Química
+    </div>
+  </div>
+
+  <div class="w3-display-container mySlides">
+    <a href=”http://localhost:8080/MaisCP2/pagina.php”>  <img src="Imagens/Portu.jpg" style="width:100%"> </a>
+    <div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">
+      Português
+    </div>
+  </div>
+
+  <div class="w3-display-container mySlides">
+    <a href=”http://localhost:8080/MaisCP2/pagina.php”>  <img src="Imagens/mat.jpg" style="width:100%"> </a>
+    <div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">
+      Matemática
+    </div>
+  </div>
+
+<div class="w3-display-container mySlides">
+    <a href=”http://localhost:8080/MaisCP2/pagina.php”><img src="Imagens/filo.jpg" style="width:100%"> </a>
+  <div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">
+    Filosofia
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+    <a href=”http://localhost:8080/MaisCP2/pagina.php”> <img src="Imagens/bio.jpg" style="width:100%"> </a>
+  <div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">
+  Biologia
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+  <a href=”http://localhost:8080/MaisCP2/pagina.php”>  <img src="Imagens/edfis.jpg" style="width:100%"> </a>
+  <div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">
+    Educação Física
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+   <a href=”http://localhost:8080/MaisCP2/pagina.php”>  <img src="Imagens/hist2.jpg" style="width:100%"> </a>
+  <div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">
+    História
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+    <a href=”http://localhost:8080/MaisCP2/pagina.php”> <img src="Imagens/info.jpg" style="width:100%"> </a>
+  <div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">
+     Informática
+  </div>
+</div>
+
+<button class="w3-button w3-display-left w3-black" onclick="plusDivs(-1)">&#10094;</button>
+<button class="w3-button w3-display-right w3-black" onclick="plusDivs(1)">&#10095;</button>
+
+</div>
+
+
+</div>
+  </div>
+      </div>
+
+<script>
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+</script>
+
+</body>
+</html>
