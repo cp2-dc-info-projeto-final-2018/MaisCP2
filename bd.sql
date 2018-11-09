4fe71a8766733f89974b1b95fadca0ae08073844
DannielSS <42175429+DannielSS@users.noreply.github.com>
2018-11-09 17:03:18 -0200

    Alterações
    aumentando o varchar da senha para (100), devido a codificação do hash.

--- bd.sql
+++ bd.sql
@@ -5,7 +5,7 @@ use MaisCP2;
 CREATE TABLE usuario(
 usuario_id int auto_increment not null,
 nomeCompleto varchar(30) not null,
-senha varchar(12) not null,
+senha varchar(100) not null,
 matricula varchar(9) not null,
 professor_id int,
 aluno_id int,
