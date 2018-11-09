a7fdc39df78505855c70d7235d6c614c7878624c
DannielSS <42175429+DannielSS@users.noreply.github.com>
2018-11-09 15:42:06 -0200

    Correção na sintaxe
    A tabela usuário estava sendo criado após a tabela aluno e a tabela professor, as mesmas possuem foreign key da tabela usuário.

--- bd.sql
+++ bd.sql
@@ -1,5 +1,15 @@
 use MaisCP2;
 
+CREATE TABLE usuario(
+usuario_id int auto_increment not null,
+nome varchar(30) not null,
+senha varchar(12) not null,
+matricula varchar(9) not null,
+professor_id int,
+aluno_id int,
+PRIMARY KEY(usuario_id)
+);
+
 CREATE TABLE aluno(
 aluno_id int not null,
 PRIMARY KEY (aluno_id),
@@ -10,16 +20,6 @@ CREATE TABLE professor(
 professor_id int not null,
 PRIMARY KEY (professor_id),
 FOREIGN KEY (professor_id) REFERENCES usuario(usuario_id)
-);
-
-CREATE TABLE usuario(
-usuario_id int auto_increment not null,
-nome varchar(30) not null,
-senha varchar(12) not null,
-matricula varchar(9) not null,
-professor_id int,
-aluno_id int,
-PRIMARY KEY(usuario_id)
 );
 
 CREATE TABLE thread(
